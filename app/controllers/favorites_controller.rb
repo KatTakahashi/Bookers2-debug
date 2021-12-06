2ef040df0f7e086183333fb5593e06db7311fe1d
EC2 Default User <ec2-user@ip-172-31-15-61.ec2.internal>
2021-12-06 08:03:47 +0000

    [Finish]6-js

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,18 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
-    #redirect_to book_path(book)　※indexでいいねしてもshowページへ飛ぶ
+    #redirect_back(fallback_location: root_path) ※JavaScriptリクエスト
+    #redirect_to book_path(book) ※indexでいいねしてもshowページへ飛ぶ ⇒ redirect_backに書き換え
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
     #redirect_to book_path(book)
   end
 
